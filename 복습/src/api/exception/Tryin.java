a22862ae8ff3a4ecc514acbc685f0161c7c06cf2
jiahn <jiahn@LAPTOP-8JS3J6C6>
2022-08-12 22:49:43 +0900

    부모 클래스

--- 복습/src/api/exception/Tryin.java
+++ 복습/src/api/exception/Tryin.java
@@ -9,15 +9,6 @@ public class Tryin {
 	public static boolean scarecrow;
 	
 	
-	public boolean isScarecrow() {
-		return scarecrow;
-	}
-
-
-	public void setScarecrow(boolean scarecrow) {
-		this.scarecrow = scarecrow;
-	}
-
 
 	//오버로딩으로 아예 검증까지 포함해서 더 줄일순없을까??? string이랑 int니까 될거같은데???
 	
