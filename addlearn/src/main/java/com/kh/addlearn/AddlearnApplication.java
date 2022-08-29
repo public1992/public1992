6fe36778f03bcad7ff9ced596aee3b0451bcdea2
public1992 <livethereality22@gmail.com>
2022-08-29 21:42:18 +0900

    Spring boot 설치/세팅

---
+++ addlearn/src/main/java/com/kh/addlearn/AddlearnApplication.java
@@ -0,0 +1,13 @@
+package com.kh.addlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddlearnApplication.class, args);
+	}
+
+}
---
+++ addlearn/src/test/java/com/kh/addlearn/AddlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.addlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
